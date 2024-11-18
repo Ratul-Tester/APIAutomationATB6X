9fd157b47b4750670e3631db36987c1a39e667b7
Ratul Nandy <ratul.tester@gmail.com>
2024-11-18 10:42:25 +0530

    Initial commit

---
+++ src/main/java/org/ecom_booking/App.java
@@ -0,0 +1,13 @@
+package org.ecom_booking;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
